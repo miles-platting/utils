c3e0374204f361fdd3132a4c21c3ba2dce30f6e3
miles-platting <38829327+miles-platting@users.noreply.github.com>
2018-05-03 09:06:10 +1000

    Removed some names

--- test-utils.cpp
+++ test-utils.cpp
@@ -1,86 +1,10 @@
-/*
-
-See Makefile YAY!!!!!!!!!!
-
-clang++ --analyze -Xanalyzer -analyzer-output=text utils.cpp -std=c++11
-
-*/
-
-/*
-debugging and instrumentation switches
-*/
 
 #include <iostream>   // 
 #include "utils.h"
-//#include "format.h"
-//#include "xml_read.h"
-//#include "sqlite3.h"
 
 using namespace std;
 using namespace std::chrono;
 
-#if 0
-int test_run(sqlite3* db) { 
-
-  string ql;
-  string s_msg;
-  string sql;
-  string s_error;
-  string s_database;
-  char *zErrMsg = 0;
-  string s_filename("fulldb.sql");
-  long i_len = 0;
-
-  int msec = 0;
-  clock_t t_start;
-  clock_t t_end;
-  int i_diff;
-
-  s_database = ":memory:";
-  t_start = clock();                               
-  sqlite3_open(s_database.c_str(), &db);
-  if (db == 0) {
-    printf("Could not open database.");
-    return 1;
-  }
-
-  ql = read_file(s_filename,s_msg,&i_len);         
-  sql_stmt(db,ql,s_error);                         
-
-  sql = "SELECT * FROM parts_attribs"; 
-  int i_recs = select_stmt_build(db, sql, s_error);
-  cout << i_recs << " records in select_stmt_build\n";
-  // look in rs
-  cout << rs.vrs_set.size() << " records in vector vrs_set\n";
-
-  string s_out;
-  //if (b_display) {
-
-    for (auto it = rs.vrs_set.begin(); it != rs.vrs_set.end(); ++it) {
-      cout << "fields " << it->size() << " ";
-      for (auto r_it = it->begin(); r_it != it->end(); r_it++) {
-          cout << *r_it << " ";
-      }
-      cout << endl;
-    }
-    cout << i_recs << endl;
-  //}
-
-  t_end = clock() - t_start;                       
-  msec = t_end * 1000 / CLOCKS_PER_SEC;
-
-  cout << i_recs << " records in select_stmt_build\n";
-  // look in rs
-  cout << rs.vrs_set.size() << " records in vector vrs_set\n";
-  s_msg = fmt::format("Create database and execute query: time taken {0}.{1} seconds)\n", 
-                        msec/1000, msec%1000
-                    );
-  cout << s_msg;
-  //cout << sizeof(rs) << endl;
-  //printf("%s\n",s_msg.c_str());
-  return 0;
-}
-#endif
 
 int main(int argc, char** argv)
 {
@@ -198,13 +122,11 @@ int main(int argc, char** argv)
 	std::time_t t = std::time(nullptr);
 
   	cout << "SPLIT TEST 2 (with string delimiter) 999\n";
-    s1 =	"David999Robert999Beech999Neill999Bostock999Hewitt999Proudlove999Sidebotham999"
+    s1 =	"David999Robert999Bostock999Hewitt999Proudlove999Sidebotham999"
     		"Dowler999"
     		"999"
-    		"Karen999Maree999Wulff999Nobbs999Gooley999"
+    		"Karen999Maree999"
     		"999"
-    		"Rachel999Celia999Edward999Clara999Eleanor999Stella999"
-    		"999Denney999Langshaw999Baron";
     s_delimiter = "999";
 	oz_utils::vector_str v2;
 
@@ -216,11 +138,6 @@ int main(int argc, char** argv)
     }
 
     cout << "Test all string handling for empty string\n\n";
-#if 0
-	sqlite3* db;
-  	test_run(db);
-  	sqlite3_close(db);
-#endif
 
 	int i_len;
 	bool bql = oz_utils::read_file("the_missing.file",s_data,s_msg);  
@@ -243,21 +160,6 @@ int main(int argc, char** argv)
 	cout << "What about a string deflate ie remove all in char set x from a string\n"
 		<< "But how would you do efficiently?\n\n";
 
-/*
-	let z = first char in set[of chars to remove]
-	foreach x in set[of chars to remove]
-		x = z
-	string.replace(x,'') 
-
-#include <algorithm>
-#include <string>
-
-void some_func() {
-  std::string s = "example string";
-  std::replace( s.begin(), s.end(), 'x', 'y'); // replace all 'x' to 'y'
-}
-	
-*/
 	string s_test;
 	oz_utils::rtrim(s_test,ws);
 	oz_utils::ltrim(s_test,ws);
